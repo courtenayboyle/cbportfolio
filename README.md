d35445d9950f7bb0a491a12c9de025357d90d6cb
Courtenay Boyle <courtenay@courtenays-air.attlocal.net>
2023-07-04 20:14:57 -0400

    added projects

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
-# cbportfolio
+# README Generator<br>
+## <b>Title: Portfolio App<br></b>
+## Description: Developed an responsive web application using HTML, CSS and Javascript to showcase projects, certifications, links to social sites and an up-to-date resume.
+<br>
+Table of Contents<br>
 
-This is my personal portfolio website that I created using HTML, CSS and Javascript.
-I also have an up to date resume available on the website.
+[Installation](##Installation)
 
-Feel free to leave feedback. Thanks!
+[Instructions](##Instructions)
 
-Link to website: https://courtenayboyle.github.io/cbportfolio/
+[Licenses](##Licenses)
+
+[Deployed](##Deployed)
+
+[Contact](##Contact)
+
+[Contributors](##Contributors)
+
+[Testing](##Testing)
+
+Installation:
+The project is available on github.
+
+Instructions:
+None
+
+Deployed:
+https://courtenayboyle.github.io/cbportfolio/
+
+Contact:
+Github: https://github.com/courtenayboyle
+
+LinkedIn: https://www.linkedin.com/in/courtenay-boyle-290707269/
+
+Email: courtenayboyle@gmail.com
+
+Contributors:
+None
+
+Tests:
+Testing was conducted locally, and deployed via github.
+Feel free to test with your next project. Let me know if you have any questions or comments via any of the methods above in contacts.
--- index.js
+++ index.js
@@ -1,84 +1,112 @@
 const cardData = [
-    {
-        heading: 'API Python Project',
-        image: './img/API_App_Project.png',
-        body: "I created a script to automatically take data from a crypto API using Python. Additionally, I did some data cleaning and created a few visualizations.",
-        buttonAddress: 'https://github.com/courtenayboyle/API-Portfolio-Project.ipynb',
-        buttonText: 'READ MORE'
-    },
-		{
-			heading: 'Page Ranking App',
-			image: './img/pagerank.gif',
-			body: "This Python program crawls a web site and pulls a series of pages into the database, recording the links between pages. It is a set of programs that emulate some of the functions of a search engine. They store their data in a SQLITE3 database. ",
-			buttonAddress: 'https://github.com/courtenayboyle/pageRank',
-			buttonText: 'READ MORE'
+	{
+		heading: 'API Python App',
+		image: './img/API_App_Project.png',
+		body: "This is a Python script to automatically take data from a crypto API. Additionally, I did some data cleaning and created a few visualizations.",
+		buttonAddress: 'https://github.com/courtenayboyle/API-Portfolio-Project.ipynb',
+		buttonText: 'READ MORE'
 	},
-    {
-        heading: 'Air BnB Visualizations',
-        image: './img/airBnbVis.png',
-        body: 'Using data from Air Bnb, I created multiple visualizations using Tableau.',
-        buttonAddress: 'https://public.tableau.com/app/profile/courtenay.boyle6979/viz/AirBnBFullProject_16834704930030/Dashboard1?publish=yes',
-        buttonText: 'Read More'
-    },
-    {
-        heading: 'Restaurant App',
-        image: './img/restaurant_app.png',
-        body: 'This is an application that I built using HTML and CSS. This was my first push into front end development!',
-        buttonAddress: 'https://courtenayboyle.github.io/restaurantHtmlCssApp/',
-        buttonText: 'Visit Site'
-    },
-		{
-			heading: 'Chiropractic App',
-			image: './img/chiro_website.png',
-			body: 'This project is the WHY  I became a Full Stack Engineer! I opened my chiropractic business in 2017 and built this website with help from Word Press. I absolutely fell in love with coding!',
-			buttonAddress: 'https://promochiropractic.com',
-			buttonText: 'Visit Site'
-		},
-    {
-        heading: 'Excel Visualizations',
-        image: './img/Excel_Visualizations.png',
-        body: 'I created multiple visualizations looking at bike sales related to different data sets. I created the visualizations in Excel.',
-        buttonAddress: 'https://github.com/courtenayboyle/Bike-Sales-Visualizations-in-Excel',
-        buttonText: 'READ MORE'
-    },
-    {
-        heading: 'Data Visualizations',
-        image: './img/Survey_Visualizations.png',
-        body: 'I created multiple visualizations in Power BI based on a survey from Data Professionals.',
-        buttonAddress: 'https://github.com/courtenayboyle/Data-Professional-Survey-Visualization',
-        buttonText: 'READ MORE'
-    },
-    {
-        heading: 'Amazon Web Scraping App',
-        image: './img/amazonWebScraping.png',
-        body: 'In this project I used Python to scrape data from Amazon with the help of BeautifulSoup and pandas. Additionally, I set up an automatic timer to pull data and keep the data in excel files, as well as automatically emailing myself when certain criteria was met.',
-        buttonAddress: 'https://github.com/courtenayboyle/Amazon-Web-Scraping',
-        buttonText: 'READ MORE'
-    },
-    {
-        heading: 'Data Cleaning Using SQL',
-        image: './img/dataCleaning.png',
-        body: 'In this project I did lots of data cleaning using SQL. I used tables that contained data regarding housing in Nashville, TN.',
-        buttonAddress: 'https://github.com/courtenayboyle/Data-Cleaning-in-SQL',
-        buttonText: 'READ MORE'
-    },
-    {
-        heading: 'COVID Data Exploration',
-        image: './img/dataExploration.png',
-        body: 'I did some data exploration looking at COVID data. I used SQL to look at the data and compare various aspects within multiple tables. In this project I used joins to combine different tables to be able to look closer at data.',
-        buttonAddress: 'https://github.com/courtenayboyle/COVID-Data-Exploration',
-        buttonText: 'READ MORE'
-    }
+	{
+		heading: 'Page Ranking App',
+		image: './img/pagerank.gif',
+		body: "This Python program crawls a web site and pulls a series of pages into the database, recording the links between pages. It is a set of programs that emulate some of the functions of a search engine. They store their data in a SQLITE3 database. ",
+		buttonAddress: 'https://github.com/courtenayboyle/pageRank',
+		buttonText: 'READ MORE'
+	},
+	{
+		heading: 'Probability Calculator',
+		image: './img/probability_calc.png',
+		body: 'This is a Python program to determine the approximate probability of drawing certain balls randomly from a hat.',
+		buttonAddress: 'https://github.com/courtenayboyle/probability_calculator',
+		buttonText: 'Read More'
+	},
+	{
+		heading: 'Time Calculator',
+		image: './img/time_calculator.png',
+		body: 'This is a Python App that takes in parameters to include a start time, a duration time, and an optional starting day of the week. I then designed a UI to take user input to calculate a return time and day.',
+		buttonAddress: 'https://time-calculator-kjoj.onrender.com',
+		buttonText: 'Visit Site'
+	},
+	{
+		heading: 'Arithmetic Formatter',
+		image: './img/arithmetic_form_pic.png',
+		body: 'Created an app in Python that receives a list of strings that are arithmetic problems and returns the problems arranged vertically and side-by-side.',
+		buttonAddress: 'https://github.com/courtenayboyle/arithmeticFormatter',
+		buttonText: 'Read More'
+	},
+	{
+		heading: 'Shape Calculator',
+		image: './img/shape_calculator.png',
+		body: 'Used object oriented programming in Python to create a Rectangle class and a Square class. The Square class is a subclass of Rectangle that inherits methods and attributes.',
+		buttonAddress: 'https://github.com/courtenayboyle/polygon_area_calculator',
+		buttonText: 'Read More'
+	},
+	{
+		heading: 'Air BnB Visualizations',
+		image: './img/airBnbVis.png',
+		body: 'Using data from Air Bnb, I created multiple visualizations using Tableau.',
+		buttonAddress: 'https://public.tableau.com/app/profile/courtenay.boyle6979/viz/AirBnBFullProject_16834704930030/Dashboard1?publish=yes',
+		buttonText: 'Read More'
+	},
+	{
+		heading: 'Restaurant App',
+		image: './img/restaurant_app.png',
+		body: 'Web application using HTML and CSS. This was my first push into front end development!',
+		buttonAddress: 'https://courtenayboyle.github.io/restaurantHtmlCssApp/',
+		buttonText: 'Visit Site'
+	},
+	{
+		heading: 'Chiropractic App',
+		image: './img/chiro_website.png',
+		body: 'This app is the WHY  I became a Full Stack Engineer! I opened my chiropractic business in 2017 and built this website with help from Word Press. I absolutely fell in love with coding!',
+		buttonAddress: 'https://promochiropractic.com',
+		buttonText: 'Visit Site'
+	},
+	{
+		heading: 'Excel Visualizations',
+		image: './img/Excel_Visualizations.png',
+		body: 'I created multiple visualizations looking at bike sales related to different data sets. I created the visualizations in Excel.',
+		buttonAddress: 'https://github.com/courtenayboyle/Bike-Sales-Visualizations-in-Excel',
+		buttonText: 'READ MORE'
+	},
+	{
+		heading: 'Data Visualizations',
+		image: './img/Survey_Visualizations.png',
+		body: 'I created multiple visualizations in Power BI based on a survey from Data Professionals.',
+		buttonAddress: 'https://github.com/courtenayboyle/Data-Professional-Survey-Visualization',
+		buttonText: 'READ MORE'
+	},
+	{
+		heading: 'Amazon Web Scraping App',
+		image: './img/amazonWebScraping.png',
+		body: 'Python app used to scrape data from Amazon with the help of BeautifulSoup and pandas. Additionally, it contains an automatic timer that pulls data and stores it in excel files. Lastly, the app was set up to automatically email myself when certain criteria was met.',
+		buttonAddress: 'https://github.com/courtenayboyle/Amazon-Web-Scraping',
+		buttonText: 'READ MORE'
+	},
+	{
+		heading: 'Data Cleaning Using SQL',
+		image: './img/dataCleaning.png',
+		body: 'In this project I did lots of data cleaning using SQL. I used tables that contained data regarding housing in Nashville, TN.',
+		buttonAddress: 'https://github.com/courtenayboyle/Data-Cleaning-in-SQL',
+		buttonText: 'READ MORE'
+	},
+	{
+		heading: 'COVID Data Exploration',
+		image: './img/dataExploration.png',
+		body: 'This app uses SQL to do data exploration of COVID datasets. Joins were used to combine different tables to better visualize the data.',
+		buttonAddress: 'https://github.com/courtenayboyle/COVID-Data-Exploration',
+		buttonText: 'READ MORE'
+	}
 ]
 
 const postContainer = document.querySelector('.card-container');
 
-const postMethods = ()=>{
-    cardData.map((postData)=>{
-        if (postData.buttonAddress){
-            const postElement = document.createElement('div');
-            postElement.classList.add('card');
-            postElement.innerHTML = `
+const postMethods = () => {
+	cardData.map((postData) => {
+		if (postData.buttonAddress) {
+			const postElement = document.createElement('div');
+			postElement.classList.add('card');
+			postElement.innerHTML = `
             <h3 class="card-heading">${postData.heading}</h3>
             <img src="${postData.image}" alt="Project Image" />
             <p class="card-body">${postData.body}</p>
@@ -86,19 +114,19 @@ const postMethods = ()=>{
             <a href=${postData.buttonAddress}>${postData.buttonText}</a>
             </div>
             `
-            postContainer.appendChild(postElement)
-        } else {
-        const postElement = document.createElement('div');
-        postElement.classList.add('card');
-        postElement.innerHTML = `
+			postContainer.appendChild(postElement)
+		} else {
+			const postElement = document.createElement('div');
+			postElement.classList.add('card');
+			postElement.innerHTML = `
         <h3 class="card-heading">${postData.heading}</h3>
         <img src="${postData.image}" alt="Project Image" />
         <p class="card-body">${postData.body}</p>
         `
-        postContainer.appendChild(postElement)
+			postContainer.appendChild(postElement)
 
-    }
-})
+		}
+	})
 }
 postMethods()
 
